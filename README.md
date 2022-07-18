8a1d39fc949db1f46456c578c8046b7dfde5bf8e
darwin-rod <darwin.rodriguezme@ug.edu.ec>
2022-07-18 09:23:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-zuq4ym
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-zuq4ym)
---
+++ ejercicios/04-tarea-tipado.ts
@@ -0,0 +1,33 @@
+// Import stylesheets
+import './style.css';
+
+interface Direccion {
+  calle: string;
+  pais: string;
+  ciudad: string;
+}
+
+interface SuperHeroe {
+  nombre: string;
+  edad: number;
+  direccion: Direccion;
+  mostrarDireccion: () => string;
+}
+
+const superHeroe: SuperHeroe = {
+  nombre: 'Spiderman',
+  edad: 30,
+  direccion: {
+    calle: 'Main St',
+    pais: 'USA',
+    ciudad: 'NY',
+  },
+  mostrarDireccion() {
+    return (
+      this.nombre + ', ' + this.direccion.calle + ', ' + this.direccion.pais
+    );
+  },
+};
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
